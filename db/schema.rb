c9d3eb215bcbf9df25eaaa1ea5324a1267f8064b
kemi-t <takaya0902@gmail.com>
2020-11-21 18:05:21 +0900

    userモデルの単体テストコード実装
    ❶Gimei gemの追加
    ❷userモデルバリデーションの追加
    　PASSWORD_REGEX
    ❸各種エラーメッセージの変更
    ❹新規登録画面、登録失敗時にエラー文章が表示されるように仕様追加

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2020_11_15_235441) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "email", default: "", null: false
+    t.string "email", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    birthday              {Faker::Date.birthday(min_age:18,max_age:65)}
+    first_name            {Gimei.first.kanji}
+    last_name             {Gimei.last.kanji}
+    first_name_kana       {Gimei.first.katakana}
+    last_name_kana        {Gimei.last.katakana}
+  end
+end
