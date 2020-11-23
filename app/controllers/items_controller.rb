920a41c1ba9b4c9445c3553d8aaf5f55925407ba
kemi-t <takaya0902@gmail.com>
2020-11-23 22:30:31 +0900

    itemsコントローラーnew,create関連の詳細設定
    ※Category(ActiveHash)のコードミスを訂正
    ※days_to_ship(ActiveHash)のコードミスを訂正
    ❶priceの金額幅バリデーション設定
    ❷itemsコントローラーnew,createアクション定義
    ❸itemsViews,index&newの変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,11 +9,7 @@ class ItemsController < ApplicationController
 
   def create
     item = Item.new(item_params)
-    if item.save
-      redirect_to root_path
-    else
-      render :new
-    end
+    item.save
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price
+    validates :price,     numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   end
 
   validates :category_id, numericality: { other_than: 1 }
